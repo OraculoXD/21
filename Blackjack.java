21c293be62397ec9182e3ea38d9de5da07477b66
Gabriel Almeida <almeida.gremista83@gmail.com>
2022-05-13 10:49:25 -0300

    Começo com duas cartas

--- Blackjack.java
+++ Blackjack.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+import java.util.Random;
 
 /**
  * Escreva uma descrição da classe Blackjack aqui.
  * 
- * @author Gabriel
+ * @author (seu nome) 
  * @version (um número da versão ou uma data)
  */
 public class Blackjack
 {
+    public static void main(String[] args) {
+        Scanner sc;
+        Random r;
+        int c1, c2;
+        
+        r = new Random();
+        
+        c1 = 1 + r.nextInt(13);
+        c2 = 1 + r.nextInt(13);
+        
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c1 + c2);
+        
+    }
 }
