9024654963c61acd982b0e3a4ca518346277191e
Gabriel Almeida <almeida.gremista83@gmail.com>
2022-05-16 10:20:44 -0300

    Criar rotina para realizar a soma com valores para J, Q e K

--- Blackjack.java
+++ Blackjack.java
@@ -11,8 +11,7 @@ public class Blackjack
     public static int gerarCarta(Random r) {
         return 1 + r.nextInt(13);
     }
-    
-    
+
     public static int valor(int c) {
         if (c > 10)
             return 10;
@@ -21,10 +20,9 @@ public class Blackjack
     }
     
     public static int somarCartas(int c1, int c2, int c3) {
-        return valor (c1) + valor(c2) + valor(c3);
+        return valor(c1) + valor(c2) + valor(c3);
     }
-    
-    
+   
     public static void main(String[] args) {
         Random r;
         int c1, c2, c3;
@@ -43,14 +41,14 @@ public class Blackjack
         System.out.printf("COMPUTADOR%n");
         System.out.printf("Carta 1: %d%n", c1);
         System.out.printf("Carta 2: %d%n", c2);
-        System.out.printf("Carta 2: %d%n", c3);
-        System.out.printf("Total  : %d%n", c1 + c2 + c3);
+        System.out.printf("Carta 3: %d%n", c3);
+        System.out.printf("Total  : %d%n", somarCartas(c1, c2, c3));
        
         System.out.printf("HUMANO%n");
         System.out.printf("Carta 1: %d%n", j1);
         System.out.printf("Carta 2: %d%n", j2);
-        System.out.printf("Carta 2: %d%n", j3);
-        System.out.printf("Total  : %d%n", j1 + j2 + j3);
+        System.out.printf("Carta 3: %d%n", j3);
+        System.out.printf("Total  : %d%n", somarCartas(j1, j2, j3));
         
     }
 }
