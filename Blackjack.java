e83819eb117e6b518f7b27e463853b3042e15515
Gabriel Almeida <almeida.gremista83@gmail.com>
2022-05-16 10:19:16 -0300

    Gerar a terceira carta

--- Blackjack.java
+++ Blackjack.java
@@ -3,33 +3,54 @@ import java.util.Random;
 /**
  * Escreva uma descrição da classe Blackjack aqui.
  * 
- * @author Gabriel Almeida
+ * @author marco.mangan@pucrs.br 
  * @version (um número da versão ou uma data)
  */
 public class Blackjack
 {
+    public static int gerarCarta(Random r) {
+        return 1 + r.nextInt(13);
+    }
+    
+    
+    public static int valor(int c) {
+        if (c > 10)
+            return 10;
+        else
+            return c;
+    }
+    
+    public static int somarCartas(int c1, int c2, int c3) {
+        return valor (c1) + valor(c2) + valor(c3);
+    }
+    
+    
     public static void main(String[] args) {
         Random r;
-        int c1, c2;
-        int j1, j2;
+        int c1, c2, c3;
+        int j1, j2, j3;
         
         r = new Random();
         
-        c1 = 1 + r.nextInt(13);
-        c2 = 1 + r.nextInt(13);
+        c1 = gerarCarta(r);
+        c2 = gerarCarta(r);
+        c3 = gerarCarta(r);
         
-        j1 = 1 + r.nextInt(13);
-        j2 = 1 + r.nextInt(13);
+        j1 = gerarCarta(r);
+        j2 = gerarCarta(r);
+        j3 = gerarCarta(r);
         
         System.out.printf("COMPUTADOR%n");
         System.out.printf("Carta 1: %d%n", c1);
         System.out.printf("Carta 2: %d%n", c2);
-        System.out.printf("Total  : %d%n", c1 + c2);
+        System.out.printf("Carta 2: %d%n", c3);
+        System.out.printf("Total  : %d%n", c1 + c2 + c3);
        
         System.out.printf("HUMANO%n");
         System.out.printf("Carta 1: %d%n", j1);
         System.out.printf("Carta 2: %d%n", j2);
-        System.out.printf("Total  : %d%n", j1 + j2);
+        System.out.printf("Carta 2: %d%n", j3);
+        System.out.printf("Total  : %d%n", j1 + j2 + j3);
         
     }
 }
